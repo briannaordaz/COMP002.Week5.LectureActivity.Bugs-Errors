9af3174ee79d41d149096206bc49e4ade55434ad
briannaordaz <briannaordaz@gmail.com>
2024-11-14 15:06:07 -0800

    fixed bugs

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let myGrades = [100, 100, 90, 73, 78, 94, 86];
+myGrades = [100, 100, 90, 73, 78, 94, 86];
 
 myAverage = 0;
-// the loop has to start at 0
 
-for (let i = 0; i <= myGrades.length; i++) {
+// the loop has to start at 0, changed i = 1 to i = 0
+//  changed "i <= myGrades.length" to "i <= 6" because 6 is the total length of the code (since it starts at 0 and ends at 6) and the output can be less than or equal to one of the array numbers 
+for (let i = 0; i <= 6; i++) {
 
 myAverage += myGrades[i];
 
@@ -11,4 +12,4 @@ myAverage += myGrades[i];
 
 myAverage = myAverage / myGrades.length;
 
-console.log("My grade average is " + myAverage);
+console.log("My grade average is " + myAverage); // output: 88.71428571428571
