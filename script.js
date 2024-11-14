e783aaaf86b28d37e87d87497675a15469e0f8b1
briannaordaz <briannaordaz@gmail.com>
2024-11-14 14:31:43 -0800

    changed the i = 1 to i = 0

---
+++ script.js
@@ -0,0 +1,14 @@
+let myGrades = [100, 100, 90, 73, 78, 94, 86];
+
+myAverage = 0;
+// the loop has to start at 0
+
+for (let i = 0; i <= myGrades.length; i++) {
+
+myAverage += myGrades[i];
+
+}
+
+myAverage = myAverage / myGrades.length;
+
+console.log("My grade average is " + myAverage);
